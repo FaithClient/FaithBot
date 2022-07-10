ad7802393f7def838f3688364a25e7e975aaa0ac
immo !! <thisspartakos@gmail.com>
2022-07-10 18:26:24 +0300

    finished the on_member_join code

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import nextcord, os
 from nextcord.ext import commands
 from dotenv import load_dotenv
 
+welcome_channel_id = 942179597112475681
+
 # intents = nextcord.Intents.default()
 # intents.members = True #DEV ENABLE ALL OF THE INTENTS!!!!!!!
 client = commands.Bot(command_prefix="f!")
@@ -15,6 +17,8 @@ async def on_ready():
 async def on_member_join(member: nextcord.Member):
     embed = nextcord.Embed(description=f"👋 {member.mention} Welcome to the server!", color=nextcord.Color.blurple())
     embed.set_thumbnail(url=member.avatar)
+    await member.guild.get_channel(welcome_channel_id).send(embed=embed)
+
 
 if __name__ == "__main__":
     load_dotenv()
