6c5038680a74398422ad6833e961b3a2cd36c20a
hebsu <rabbithole.bali@gmail.com>
2022-07-11 11:16:28 +0800

    fun

--- README.md
+++ README.md
@@ -4,5 +4,8 @@ Discord Bot for FaithClient
 Made by - immo, Hebiscuit, devgocri
 
 ## TODO
-- Legit enable the intents
-- Test everything we have made
+- Legit enable the intents -- done
+- Make the welcome message an image
+- Immo - Make the download command
+- Make Cogs know what bot.user does
+- FIX HELP COMMAND (CUSTOM)
--- main.py
+++ main.py
@@ -1,42 +1,55 @@
+# Imports
 import nextcord, os
 import datetime
-
 from nextcord.ext import commands
 from dotenv import load_dotenv
 
+# Variables
 welcome_channel_id = 942179597112475681
 year = datetime.date.today().year
-# intents = nextcord.Intents.default()
-# intents.members = True #DEV ENABLE ALL OF THE INTENTS!!!!!!!
-client = commands.Bot(command_prefix='f!', status=nextcord.Status.idle)
+intents = nextcord.Intents.all() 
+intents.members = True
+color = 0xffd500
 
-@client.event
+# Bot Initialization
+bot = commands.Bot(command_prefix='f!', intents=intents)
+
+
+# Bot On Ready
+@bot.event
 async def on_ready():
-    print(f"successfully logged in as {client.user}")
-    await client.change_presence(activity=nextcord.Activity(type=nextcord.ActivityType.playing, name="f!help | FaithClient!"))
+    print(f"successfully logged in as {bot.user}")
+    await bot.change_presence(status=nextcord.Status.idle, activity=nextcord.Activity(type=nextcord.ActivityType.watching, name="f!help | FaithClient!"))
 
-@client.event
+# Detect Joining Members
+@bot.event
 async def on_member_join(member: nextcord.Member):
-    embed = nextcord.Embed(description=f"👋 {member.mention} Welcome to the server!", color=nextcord.Color.blurple())
-    embed.set_thumbnail(url=member.avatar)
+    embed = nextcord.Embed(title="Heyo there!", description=f"👋 {member.mention} Welcome to the server!\n\nWe hope you have a great time in here :D", color=color)
+    embed.set_thumbnail(url=member.default_avatar)
     await member.guild.get_channel(welcome_channel_id).send(embed=embed)
 
 
-@client.command()
+# Basic Ping Command
+@bot.command()
 async def ping(interaction):
-    footer = f"Requested by {interaction.user}"
     embed = nextcord.Embed(
-        color=0x5539cc,
+        color=color,
         title="Ping Pong! 🏓",
-        
     )
-    embed.add_field(name='Bot Latency!', value=f"Bot Ping is **{round(client.latency * 1000)}ms**", inline=True)
-    embed.set_author(name=client.user, icon_url=client.user.avatar)
-    embed.set_footer(text=footer)
-    embed.set_thumbnail(url=client.user.avatar)
+    embed.add_field(name='Bot Latency!', value=f"Bot ping is **{round(bot.latency * 1000)}ms**", inline=True)
+    embed.set_author(name=bot.user, icon_url=bot.user.avatar)
+    embed.set_thumbnail(url=bot.user.avatar)
+    embed.set_footer(text=f"Requested by {interaction.message.author}")
     embed.timestamp = datetime.datetime.utcnow()
     await interaction.send(embed=embed)
 
+# Initialize Cogs
+for filename in os.listdir("./cogs"):
+    if filename.endswith(".py"):
+        bot.load_extension(f"cogs.{filename[:-3]}")
+        print(f"Successfully loaded {filename}")
+
+# Runs Bot
 if __name__ == "__main__":
     load_dotenv()
-    client.run(os.getenv("DISCORD_TOKEN"))
+    bot.run(os.getenv("DISCORD_TOKEN"))
