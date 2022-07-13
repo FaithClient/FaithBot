f3fb54c63f094760295b9f3870d5cff32a8fb648
immo !! <thisspartakos@gmail.com>
2022-07-13 17:41:56 +0300

    made an important command :) (lols)

--- cogs/important.py
+++ cogs/important.py
@@ -6,6 +6,7 @@ from nextcord.ext.commands import Context
 class Important(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+        self.d_ch_id = 2
     
     @commands.Cog.listener()
     async def on_command_error(self, ctx: Context, error: Exception):
@@ -22,13 +23,29 @@ class Important(commands.Cog):
             await msg.delete()
             return
         pictures = [p.url for p in ctx.message.attachments]
-        embed = nextcord.Embed(title=f"{title} {ver}", description=f"{description}\nDownload Link: {link}", color=nextcord.Color.brand_green())
+        embed = nextcord.Embed(title=f"{title} {ver}", description=f"{description}", color=nextcord.Color.brand_green())
+        embed.add_field(
+            name = "Download 🔽",
+            value=f"[Click here to download]({link})",
+            inline=False
+        )
         embed.set_thumbnail(url=self.bot.user.avatar)
-        #embed.set_image()
+        embed.set_image(url=pictures[0] if len(pictures) > 0 else self.bot.user.avatar)
         embed.set_footer(text=f"Announced by {ctx.author}")
         embed.timestamp = datetime.datetime.now()
         msg = await ctx.send(embed=embed)
         await msg.add_reaction("▶")
+    
+    # @commands.Cog.listener()
+    # async def on_reaction_add(self, reaction: nextcord.Reaction, member: nextcord.User):
+    #     if member != self.bot.user:
+    #         if reaction.message.channel.id == 
+
+    @commands.command()
+    @commands.has_role("Bot Developer")
+    async def giverole(self, ctx: Context, role: nextcord.Role, member: nextcord.Member):
+        await member.add_roles(role)
+        await ctx.send(f"{ctx.author.mention} {role.mention} was given to {member.mention}")
 
 def setup(bot: commands.Bot):
     bot.add_cog(Important(bot))
